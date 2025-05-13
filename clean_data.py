088186c7bda926c70ec57a4b4ca886ed357f7a27
IrynaFrascarolo <iryna.frascarolo@gmail.com>
2025-05-13 16:29:47 +0200

    rrrrr

--- clean_data.py
+++ clean_data.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 import glob
 
-base_folder = '/home/appuser/'
+base_folder = os.path.expanduser('~/Qimproject/')
 
 def clean_data(df):
     # Ensure the 'Error Code' column is treated as string to accommodate 'No Error'
